4bc571cd67abecf52f72218be10310da3cb6abfc
Saurabh Maheshwari <maheshwari.saurabh@outlook.com>
2014-03-18 16:40:37 -0600

    refactoring

---
+++ logAnalysis-client/src/main/java/com/heliosmi/logging/sender/LogSender.java
@@ -0,0 +1,15 @@
+package com.heliosmi.logging.sender;
+
+import org.springframework.jms.core.JmsTemplate;
+
+/**
+ * Sender class to transfer message to ActiveMQ.
+ * 
+ * @author Saurabh Maheshwari
+ *
+ */
+public class LogSender {
+    private JmsTemplate jmsTemplate;
+    
+
+}
