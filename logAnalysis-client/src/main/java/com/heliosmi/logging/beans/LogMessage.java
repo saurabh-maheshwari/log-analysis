6fa65bf4b96dcca18c868851c77a721f95198e5f
Saurabh Maheshwari <maheshwari.saurabh@outlook.com>
2014-03-18 16:04:24 -0600

    first commit

---
+++ logAnalysis-client/src/main/java/com/heliosmi/logging/beans/LogMessage.java
@@ -0,0 +1,206 @@
+/*******************************************************************************
+ * Copyright 2014 Saurabh Maheshwari
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package com.heliosmi.logging.beans;
+
+import java.util.Date;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.heliosmi.logging.beans.LogMessage.Builder;
+
+/**
+ * Message bean for persistence. Utilizes a builder pattern for bean creation.
+ * 
+ * <p>
+ * createdDate is number of milliseconds since January 1, 1970.
+ * 
+ * @author Saurabh Maheshwari
+ * 
+ */
+public class LogMessage extends BaseBean {
+
+    private String threadID;
+    private String correlationID;
+    private String className;
+    private String methodName;
+    private String packageName;
+    private String applicationName;
+    private String hostName;
+    private long createdDate;
+    private long duration;
+    private String request;
+    private String response;
+    private boolean errorYN;
+    private String errorStacktrace;
+
+    public static class Builder {
+
+        private static final int MAX_REQUEST_RESPONSE_ERRORSTACKTRACE_LENGTH = 1000;
+        private static final int MAX_FIELD_LENGTH = 50;
+
+        private String threadID;
+        private String correlationID;
+        private String className;
+        private String methodName;
+        private String packageName;
+        private String applicationName;
+        private String hostName;
+        private long createdDate;
+        private long duration;
+        private String request;
+        private String response;
+        private boolean errorYN;
+        private String errorStacktrace;
+
+        public LogMessage build() {
+            createdDate = new Date().getTime();
+            return new LogMessage(this);
+        }
+
+        public Builder threadID(String strValue) {
+            threadID = truncate(strValue, MAX_FIELD_LENGTH);
+            return this;
+        }
+
+        public Builder correlationID(String strValue) {
+            correlationID = truncate(strValue, MAX_FIELD_LENGTH);
+            return this;
+        }
+
+        public Builder className(String strValue) {
+            className = truncate(strValue, MAX_FIELD_LENGTH);
+            return this;
+        }
+
+        public Builder methodName(String strValue) {
+            methodName = truncate(strValue, MAX_FIELD_LENGTH);
+            return this;
+        }
+
+        public Builder hostName(String strValue) {
+            hostName = truncate(strValue, MAX_FIELD_LENGTH);
+            return this;
+        }
+
+        public Builder duration(int intValue) {
+            duration = intValue;
+            return this;
+        }
+
+        public Builder request(String string) {
+            request = truncate(string, MAX_REQUEST_RESPONSE_ERRORSTACKTRACE_LENGTH);
+            return this;
+        }
+
+        public Builder packageName(String string) {
+            packageName = truncate(string, MAX_FIELD_LENGTH);
+            return this;
+        }
+
+        public Builder response(String string) {
+            response = truncate(string, MAX_REQUEST_RESPONSE_ERRORSTACKTRACE_LENGTH);
+            return this;
+        }
+
+        public Builder errorYN(Boolean boolValue) {
+            errorYN = boolValue;
+            return this;
+        }
+
+        public Builder errorStacktrace(String string) {
+            errorStacktrace = truncate(string, MAX_REQUEST_RESPONSE_ERRORSTACKTRACE_LENGTH);
+            return this;
+        }
+
+        public Builder applicationName(String string) {
+            applicationName = truncate(string, MAX_FIELD_LENGTH);
+            return this;
+        }
+
+        private String truncate(String string, int maxValue) {
+            return StringUtils.substring(string, 0, maxValue);
+        }
+    }
+
+    private LogMessage(Builder builder) {
+        createdDate = builder.createdDate;
+        threadID = builder.threadID;
+        correlationID = builder.correlationID;
+        className = builder.className;
+        methodName = builder.methodName;
+        hostName = builder.hostName;
+        applicationName = builder.applicationName;
+        duration = builder.duration;
+        request = builder.request;
+        response = builder.response;
+        packageName = builder.packageName;
+        errorYN = builder.errorYN;
+        errorStacktrace = builder.errorStacktrace;
+    }
+
+    public String getThreadID() {
+        return threadID;
+    }
+
+    public String getCorrelationID() {
+        return correlationID;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public String getHostName() {
+        return hostName;
+    }
+
+    public long getCreatedDate() {
+        return createdDate;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public String getRequest() {
+        return request;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public boolean isErrorYN() {
+        return errorYN;
+    }
+
+    public String getErrorStacktrace() {
+        return errorStacktrace;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public String getApplicationName() {
+        return applicationName;
+    }
+
+}
---
+++ logAnalysis-client/src/test/java/com/heliosmi/logging/beans/LogMessageTest.java
@@ -0,0 +1,51 @@
+package com.heliosmi.logging.beans;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.Test;
+
+public class LogMessageTest {
+
+    @Test
+    public void testCreation() {
+        LogMessage logMessage = new LogMessage.Builder().threadID("threadName").correlationID("correlationID")
+                .className("className").methodName("methodName").applicationName("applicationName")
+                .hostName("hostName").duration(3).request("request").packageName("packageName").response("response")
+                .errorYN(Boolean.FALSE).errorStacktrace("errorStacktrace").build();
+
+        assertNotNull(logMessage.getCreatedDate());
+        assertTrue(logMessage.getDuration() == 3);
+        assertTrue(logMessage.getThreadID().equals("threadName"));
+        assertTrue(logMessage.getCorrelationID().equals("correlationID"));
+        assertTrue(logMessage.getClassName().equals("className"));
+        assertTrue(logMessage.getMethodName().equals("methodName"));
+        assertTrue(logMessage.getApplicationName().equals("applicationName"));
+        assertTrue(logMessage.getHostName().equals("hostName"));
+        assertTrue(logMessage.getRequest().equals("request"));
+        assertTrue(logMessage.getPackageName().equals("packageName"));
+        assertTrue(logMessage.getResponse().equals("response"));
+        assertFalse(logMessage.isErrorYN());
+        assertTrue(logMessage.getErrorStacktrace().equals("errorStacktrace"));
+    }
+
+    @Test
+    public void testTruncate_Request_Response_ErrorStacktrace() {
+
+        LogMessage logMessage = new LogMessage.Builder().request(RandomStringUtils.random(5000))
+                .response(RandomStringUtils.random(5000)).errorStacktrace(RandomStringUtils.random(5000)).build();
+
+        assertTrue(logMessage.getRequest().length() == 1000);
+        assertTrue(logMessage.getResponse().length() == 1000);
+        assertTrue(logMessage.getErrorStacktrace().length() == 1000);
+    }
+
+    @Test
+    public void testTruncate_Method() {
+        LogMessage logMessage = new LogMessage.Builder().methodName(RandomStringUtils.random(500)).build();
+
+        assertTrue(logMessage.getMethodName().length() == 50);
+    }
+}
